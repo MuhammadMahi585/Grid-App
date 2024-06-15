b4475414973ef02bf9c28316a9957d5cd02d0430
MuhammadMahi585 <maheemahesar39@gmail.com>
2024-06-15 15:30:58 -0700

    complete app

--- app/src/main/java/com/example/grid/MainActivity.kt
+++ app/src/main/java/com/example/grid/MainActivity.kt
@@ -6,10 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,9 +28,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -51,93 +62,57 @@ fun ListView() {
 
 @Composable
 fun ListApp(topic: List<Topic>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier.padding(2.dp)) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier
+
+    ){
         items(topic) { af ->
-            ListCard(topic = af, modifier = Modifier.padding(8.dp))
+            ListCard(topic = af, modifier = Modifier.padding(2.dp))
         }
     }
 }
 
 @Composable
 fun ListCard(topic: Topic, modifier: Modifier = Modifier) {
-    Row{
-    Column(modifier = modifier.weight(1f)) {
-        Row {
-            Image(
-                painter = painterResource(id = topic.imageId),
-                contentDescription = stringResource(id = topic.description),
-                modifier = Modifier
-                    .height(68.dp)
-                    .width(68.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    ,
+    Card {
+        Row (modifier = Modifier.padding(4.dp)){
+            Box {
+                        Image(
+                            painter = painterResource(id = topic.imageId),
+                            contentDescription = stringResource(id = topic.description),
+                            modifier = Modifier
+                                .height(68.dp)
+                                .width(68.dp)
+                                .clip(RoundedCornerShape(8.dp)),
 
-                contentScale = ContentScale.Fit
-            )
-            Column(
-                modifier = Modifier
-                    .background(color = colorResource(id = R.color.grid))
-                    .width(120.dp)
-                    .height(68.dp)
-            ) {
-                Text(
-                    text = stringResource(id = topic.description),
-                    style = MaterialTheme.typography.labelSmall,
-                    maxLines = 1,
-                    modifier = Modifier
-                        .padding(top = 16.dp, end = 16.dp, start = 16.dp)
-                )
-                Row(modifier = Modifier.padding(top = 8.dp)) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_grain_24),
-                        contentDescription = "icon",
-                        modifier = Modifier.padding(start = 16.dp, end = 8.dp)
-                    )
-                    Text(
-                        text = topic.num.toString()
-                    )
-                }
+                            contentScale = ContentScale.Fit
+                        )
             }
-        }
-    }
-        Column(modifier = modifier.weight(1f)) {
-            Row {
-                Image(
-                    painter = painterResource(id = topic.imageId),
-                    contentDescription = stringResource(id = topic.description),
-                    modifier = Modifier
-                        .height(68.dp)
-                        .width(68.dp)
-                        .clip(RoundedCornerShape(8.dp))
-                    ,
-
-                    contentScale = ContentScale.Fit
-                )
-                Column(
-                    modifier = Modifier
-                        .background(color = colorResource(id = R.color.grid))
-                        .width(120.dp)
-                        .height(68.dp)
-                ) {
-                    Text(
-                        text = stringResource(id = topic.description),
-                        style = MaterialTheme.typography.bodySmall,
-                        maxLines = 1,
-                        modifier = Modifier
-                            .padding(top = 16.dp, end = 16.dp, start = 16.dp)
-                    )
-                    Row(modifier = Modifier.padding(top = 8.dp)) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.baseline_grain_24),
-                            contentDescription = "icon",
-                            modifier = Modifier.padding(start = 16.dp, end = 8.dp)
-                        )
-                        Text(
-                            text = topic.num.toString()
-                        )
+                        Column(
+                            modifier = Modifier
+                                .background(color = colorResource(id = R.color.grid))
+                                .width(120.dp)
+                                .height(68.dp)
+                        ) {
+                            Text(
+                                text = stringResource(id = topic.description),
+                                style = MaterialTheme.typography.labelSmall,
+                                maxLines = 1,
+                                modifier = Modifier
+                                    .padding(top = 16.dp, end = 16.dp, start = 16.dp)
+                            )
+                            Row(modifier = Modifier.padding(top = 8.dp)) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.baseline_grain_24),
+                                    contentDescription = "icon",
+                                    modifier = Modifier.padding(start = 16.dp, end = 8.dp)
+                                )
+                                Text(
+                                    text = topic.num.toString()
+                                )
+                            }
+                        }
                     }
                 }
             }
-        }
-    }
-}
